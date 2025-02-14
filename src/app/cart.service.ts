a67afe2a6986c1a62b6ee43b17165c59b8bd7768
DavidAlonso858 <davidmadrid858@gmail.com>
2025-02-14 10:23:12 +0100

    hecho el proveedores y la forma para ejecutar json y proyecto a la vez

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+import { Proveedor } from './proveedor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  private url = 'http://localhost:3000/proveedores';
+
   items: Product[] = [];
   // items: {product: Product, quantity: number }[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+
+  }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,12 +25,16 @@ export class CartService {
     return this.items;
   }
 
+  getProveedores() {
+    return this.http.get<Proveedor[]>(this.url);
+  }
+
   clearCart() {
     this.items = [];
     return this.items;
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
   }
 }
