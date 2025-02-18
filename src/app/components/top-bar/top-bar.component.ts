801c44cafbf7c7ec4eb364fbecd85a9a40bce2b9
david <dalogar0801@g.educaand.es>
2025-02-18 20:26:36 +0100

    numero carrito (service y nav)

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-top-bar',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './top-bar.component.html',
     styleUrls: ['./top-bar.component.css'],
 })
