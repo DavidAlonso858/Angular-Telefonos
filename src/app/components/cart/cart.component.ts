f9c780c18fade56c3d45750e6b5a670c41e20daa
david <dalogar0801@g.educaand.es>
2025-02-18 19:42:27 +0100

    select de opcion de precio en el carro

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,25 +1,35 @@
 import { Component } from '@angular/core';
-import { FormBuilder,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-cart',
-    standalone: true,
-    imports: [ReactiveFormsModule, CommonModule, RouterModule], // para usar el FormGroup
-    templateUrl: './cart.component.html',
-    styleUrl: './cart.component.css',
+  selector: 'app-cart',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterModule], // para usar el FormGroup
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css',
 })
 
 export class CartComponent {
 
   // array de productos que he seleccioando para el carro
   items = this.cartService.getItems();
-  checkoutForm = this.formBuilder.group({ name: '', address: '' });
-  
-  constructor(private cartService: CartService, private formBuilder: FormBuilder) { }
+
+  checkoutForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    shipping: new FormControl('', [Validators.required])
+  });
+
+  shippingCosts!: Observable<{ type: string, price: number }[]>;
+
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {
+    this.shippingCosts = this.cartService.getShippingPrices();
+  }
 
   onSubmit(): void {
     // limpia los productos del carrito declarando el array a vacio
